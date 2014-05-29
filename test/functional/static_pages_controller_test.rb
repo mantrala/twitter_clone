d4a18e6fbe70f7613037edb5f14f963a08fd5057
Sarath Mantrala <sarath.mantrala@gmail.com>
2014-05-28 20:20:57 -0700

    finish static pages

---
+++ test/functional/static_pages_controller_test.rb
@@ -0,0 +1,25 @@
+require_relative '../test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  def test_home
+    get :home
+    assert_response(:success, :template => "home")
+    assert_select('h1', 'Sample App')
+    assert_select('title', 'TwitterClone | Home')
+  end
+
+  def test_help
+    get :help
+    assert_response(:success, :template => "help")
+    assert_select('h1', 'Help')
+    assert_select('title', 'TwitterClone | Help')
+  end
+
+  def test_about
+    get :about
+    assert_response(:success, :template => "about")
+    assert_select('h1', 'About Us')
+    assert_select('title', 'TwitterClone | About Us')
+  end
+
+end
