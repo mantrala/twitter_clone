3eaf6be308802dca558227f4d2353d40bb1625f5
Sarath Mantrala <sarath.mantrala@gmail.com>
2014-05-28 18:37:25 -0700

    add static pages controller

--- Gemfile
+++ Gemfile
@@ -5,8 +5,15 @@ gem 'rails', '3.2.16'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+group :development, :test do
+  gem 'sqlite3', '1.3.8'
+  gem 'rspec-rails', '2.13.1'
+end
 
+group :test do
+  gem 'selenium-webdriver', '2.35.1'
+  gem 'capybara', '2.1.0'
+end
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,19 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TwitterClone::Application.config.secret_token = '04bf6ccbb9920afdd6794c036db7a99b3c64d7f1ecdd17904e62619880c264fb74706937ed1adbf7e6ed6bf23a1a03d6f51c07d7afae6268fa7b5e2b3899b30f'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+TwitterClone::Application.config.secret_token = secure_token
+# TwitterClone::Application.config.secret_token = '04bf6ccbb9920afdd6794c036db7a99b3c64d7f1ecdd17904e62619880c264fb74706937ed1adbf7e6ed6bf23a1a03d6f51c07d7afae6268fa7b5e2b3899b30f'
