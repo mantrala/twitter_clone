e7bafb4eab98a2f31331097f7d8a84b2a4415ed7
Sarath Mantrala <sarath.mantrala@gmail.com>
2014-06-21 12:14:14 -0700

    finish user edit, update, index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, :only => [:edit, :update, :index]
+  before_filter :correct_user,   :only => [:edit, :update]
+
+  def index
+    @users = User.paginate(:page => params[:page])
+  end
   def new
     @user = User.new
   end
@@ -15,6 +21,30 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] ="Profile updated!"
+      sign_in(user)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def signed_in_user
+    redirect_to signin_url, :notice => "Please sign in" unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user)
   end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,4 +21,8 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
+  def current_user?(user)
+    current_user == user
+  end
+
 end
